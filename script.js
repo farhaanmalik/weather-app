9e96145b33d5b6f667311f91e8da9f4d4367cba3
Farhaan Malik <109364386+farhaanmalik@users.noreply.github.com>
2023-01-24 21:50:50 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const temp = document.getElementById("temp");
+const humidity = document.getElementById("humidity");
+const feels_like = document.getElementById("feels_like");
+const wind_speed = document.getElementById("wind_speed");
+const cityName = document.getElementById("cityName");
+
+
 const options = {
 	method: 'GET',
 	headers: {
@@ -6,61 +13,60 @@ const options = {
 	}
 };
 
+
+// fetching API, get data from JSON
 const getWeather = (city) => {
-	cityName.innerHTML = city 
 	fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
 		.then(response => response.json())
 		.then(response => {
-			console.log(response)
-
-			temp.innerHTML = response.temp
-			feels_like.innerHTML = response.feels_like
-			humidity.innerHTML = response.humidity
-			wind_speed.innerHTML = response.wind_speed
+			return showWeather(response)
 		})
-		.catch(err => console.error(err));
+		.catch(err =>
+			console.error(err)
+		)
 }
 
-// Searching other location weather
+// default text showing before getting weather of city
+weather.innerHTML = `<h3 style="color:white; padding: 20px 5px;">Search and get the live weather of any city...!</h3>`
+
+
+const showWeather = (response) => {
+
+	// If the city doesn't exist in API then showing error
+	if (response.error == 'An unexpected error occured.') {
+		weather.innerHTML = `<h3 style="color:red; padding-top: 20px;">${city.value} city doesn't found</h3>`;
+		return
+	}
+
+	// Display weather on screen
+	weather.innerHTML = `<div class="col-1">
+		<img src="img/cloud.png" alt="">
+		<div class="weather-info">
+			<div class="col-1-1">
+				<span class="num" id="temp">${response.temp}</span>
+				<span class="deg"> &#176C</span>
+			</div>
+			<div class="col-1-2">
+				<span class="city" id="cityName">${city.value}</span>
+			</div>
+		</div>
+	</div>
+	<div class="col-2">
+	   <div class="weather-details">
+			<h3>Weather Details</h3>
+			<div class="w-bottom">
+				<ul>
+					<li>Humidity: <span id="humidity">${response.humidity}</span>%</li>
+					<li>Feels like: <span id="feels_like">${response.feels_like}</span>&#176C</li>
+					<li>Wind: <span id="wind_speed"></span>${response.wind_speed} km/h</li>
+				</ul>
+			</div>
+	   </div>
+	</div>`;
+}
+
+// Search other city weather
 searchCity.addEventListener("click", (event) => {
 	event.preventDefault()
-	if(city.value == ""){
-		alert("Write a city name !")
-	}
-	else{
-		getWeather(city.value);
-	}
-	myForm.reset()
+	getWeather(city.value)
 })
-
-// Default location Lucknow
-getWeather("Lucknow"); 
-
-
-// Getting current date 
-const currentDate = () => {
-	let date = new Date();
-	let month = date.getMonth();
-	let curr_date = date.getDate();
-	let year = date.getFullYear();
-	let month_name = [
-		"January",
-		"February",
-		"March",
-		"April",
-		"May",
-		"June",
-		"July",
-		"August",
-		"September",
-		"October",
-		"November",
-		"December"
-	];
-	curr_date = updateTime(curr_date);
-	document.querySelector("#full__date").innerHTML = `${curr_date} ${month_name[month]}, ${year}`;
-}
-function updateTime(date) {
-	  return date;
-}
-currentDate()
